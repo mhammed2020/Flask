54a4633a3e3db97f001d48501e10e327cbe0d924
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-08-07 15:42:56 +0100

    add static files

--- test1.py
+++ test1.py
@@ -1,14 +1,20 @@
-from flask import  Flask,render_template
+from flask import Flask, render_template
+
 app1 = Flask(__name__)
 
+
 @app1.route("/")
-
 def product_list():
+    return render_template("index.html", title="Home | ",attr2 = " paragraph from index html")
 
-    return render_template("index.html",title = "Home | ")
 @app1.route("/blog")
 def product_detail():
-    return render_template("detail.html", dtitle = " More detail | ")
+    return render_template("detail.html", title=" More detail | ", attr2 = " paragraph from detail html")
 
-if __name__ =="__main__" :
-    app1.run(debug=True,port=9999)
+@app1.route("/new")
+def add_blog():
+    return render_template("add.html", title=" new blog | ")
+
+
+if __name__ == "__main__":
+    app1.run(debug=True, port=9999)
